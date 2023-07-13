c59243630f8f76a81b70e0d7481fa00bee207b07
John (Home-PC) <jenya.s97@mail.ru>
2023-07-13 22:23:04 +0300

    add js

---
+++ index.js
@@ -0,0 +1,79 @@
+//Задание 1 
+/*console.log('Запускаем таймер');
+alert(5)
+alert(4)
+alert(3)
+alert(2)
+alert(1)
+alert(0)
+console.log('Обратный отчет закончен');
+*/
+
+//Задание 2
+/*
+alert('Брендан Айк');
+alert('10 лет');
+alert('в 1995 году');
+alert('JavaScript это не Java');
+alert('ECMAScript');
+*/
+
+//Задание 3
+/*
+prompt('Сколько будет 2 + 2?');
+prompt('Чему равен корень из 9?');
+prompt('Сколько будет 2 в 5-й степени?');
+*/
+
+//Задание 4
+/*
+let myName = 'John';
+let mySurname = "Sultan";
+let myFavoriteDrink = "Apple juice"
+let myFavoriteAnimal = "Cat and Dog";
+let myFavoriteProgrammingLanguage = "Russian";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
+*/
+
+//Задание 5\
+/*
+const carName = "lexus";
+let maxCarSpeed = '280 km/h';
+let carOwner = "John Sultan";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = '279 km/h';
+carOwner = 'John Sultan 1';
+console.log("У машины изменилась скорость", maxCarSpeed);
+console.log("У машины изменился владелец", carOwner);
+
+maxCarSpeed = '278 km/h';
+carOwner = 'John Sultan 2';
+console.log("У машины изменилась скорость", maxCarSpeed);
+console.log("У машины изменился владелец", carOwner);
+
+maxCarSpeed = '277 km/h';
+carOwner = 'John Sultan 3';
+console.log("У машины изменилась скорость", maxCarSpeed);
+console.log("У машины изменился владелец", carOwner);
+
+const question1 = 'Какая самая маленькая страна мира по населению?';
+const question2 = 'Какая самая большая страна мира по населению?';
+const question3 = 'Какой самый большой материк на планете?';
+
+const answer1 = 'Китай';
+const answer2 = 'Евразия';
+const answer3 = 'Ватикан';
+
+console.log(question1, 'Ответ:', answer3);
+console.log(question2, 'Ответ:', answer1);
+console.log(question3, 'Ответ:', answer2); 
+*/
